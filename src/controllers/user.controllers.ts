7b228f44a67def723a6beaf963367b311b1d2009
YY313@ccs.local <YY313@ccs.local>
2025-03-25 21:13:26 +0800

    Add user creation functionality

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,7 +1,8 @@
 import { AppDataSource } from "../data-source";
-import { User } from "../entities/User";
+import { User } from "../entities/user";
 import { Request, Response } from "express";
 
+const userRepository = AppDataSource.getRepository(User);
 export const createUser = async (req: Request, res: Response) => {
     const newUser = await userRepository.create({
         name: req.body.name,
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,7 @@
 import { Router } from "express"
-
+import {createUser} from "../controllers/user.controllers";
 
 export const userRouter = Router();
+
+//CREATE ONE USER
+userRouter.post('/', createUser);
