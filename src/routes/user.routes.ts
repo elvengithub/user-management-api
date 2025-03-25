6fda54d87c43992f9ff4c54ed5ef59b3e306e280
YY317@ccs.local <YY317@ccs.local>
2025-03-25 20:38:30 +0800

    Ika tulo nani

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,18 +1,4 @@
 import { Router } from "express"
-import  {deleteUser}  from "../controllers/user.controllers"
-import { getAllUsers, getUserById, } from "../controllers/user.controllers";
-import {createUser} from "../controllers/user.controllers";
 
-export const userRouter = Router()
 
-//DELETE ONE USER
-userRouter.delete('/:id', deleteUser)
-
-// GET ALL USERS
-userRouter.get("/", getAllUsers);
-
-// GET ONE USER BY ID
-userRouter.get("/:id", getUserById);
-
-// CREATE ONE USER
-userRouter.post('/', createUser);
+export const userRouter = Router();
