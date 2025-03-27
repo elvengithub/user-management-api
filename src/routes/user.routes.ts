0b19e64ee5b6c228d5bbd6d3298320dfa425b0ce
elvengithub <131513934+elvengithub@users.noreply.github.com>
2025-03-27 15:24:56 +0800

    Update user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,18 @@
 import { Router } from "express"
+import  {deleteUser}  from "../controllers/user.controllers"
+import { getAllUsers, getUserById, } from "../controllers/user.controllers";
 import {createUser} from "../controllers/user.controllers";
 
-export const userRouter = Router();
+export const userRouter = Router()
 
-//CREATE ONE USER
+//DELETE ONE USER
+userRouter.delete('/:id', deleteUser)
+
+// GET ALL USERS
+userRouter.get("/", getAllUsers);
+
+// GET ONE USER BY ID
+userRouter.get("/:id", getUserById);
+
+// CREATE ONE USER
 userRouter.post('/', createUser);
