23f2ea51ce67f14409098a9bd953c6f436625d60
unknown <YY321@ccs.local>
2025-03-27 15:57:21 +0800

    changed import file

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,5 +1,5 @@
 import { AppDataSource } from "../data-source";
-import { User } from "../entities/User";
+import { User } from "../entities/user";
 import { Request, Response } from "express";
 
 const userRepository = AppDataSource.getRepository(User);
